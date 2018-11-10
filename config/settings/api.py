09166932ddb5abb6ae1bba3ff660bb122fc1dded
Bruce Wade <bruce.wade@gmail.com>
2018-11-10 14:34:49 -0800

    #3 fixed some config settings, disabled some pylint warnings for settings * import.

--- config/settings/api.py
+++ config/settings/api.py
@@ -1,7 +1,7 @@
 """
 Django settings for stripped Virtual Spaces API.
 """
-from .common import *
+from config.settings.common import *  # pylint: disable=wildcard-import, W0614
 
 CORE_APPS = [
     'django.contrib.auth',
--- virtualspaces/account/models.py
+++ virtualspaces/account/models.py
@@ -71,4 +71,3 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
 def jwt_get_secret_key(user_model):
     return user_model.jwt_secret
-
